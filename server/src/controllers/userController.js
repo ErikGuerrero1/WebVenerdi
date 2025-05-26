5cc9fe3173b041cb728c2bc6ae846caf34309b5c
Fernando García <148652897+FerGaram@users.noreply.github.com>
2025-05-26 07:12:10 -0600

    User: logIn añadido, createUser modificado
    Se añadió logIn para usuario; se cambió el parámetro de entrada de la contraseña para createUser

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,6 +1,13 @@
 const { pool } = require('../config/database.js');
 const bcrypt = require('bcrypt');
 
+const getPasswordHash = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    return passwordHash;
+}
+
 // Params: None
 // Body: None
 const getAllUsers = async (req, res) => {
@@ -52,14 +59,21 @@ const getUserById = async (req, res) => {
 }
 
 // Params: None
-// Body: Name, Email, Phone, Address, PasswordHash (password original, el hash se hace en esta función), CreatedAt
+// Body: Name, Email, Phone, Address, Password (password original, el hash se hace en esta función), CreatedAt
 const createUser = async (req, res) => {
     try {
-        const salt = await bcrypt.genSalt(10);
-        req.body.PasswordHash = await bcrypt.hash(req.body.PasswordHash, salt);
+        const PasswordHash = await getPasswordHash(req.body.Password);
+        const nuevoUsuario = {
+            Name: req.body.Name.trim(),
+            Email: req.body.Email.trim(),
+            Phone: req.body.Phone.trim(),
+            Address: req.body.Address.trim(),
+            PasswordHash: PasswordHash,
+            CreatedAt: req.body.CreatedAt
+        };
 
         const query = `INSERT INTO user SET ?`;
-        const resp = await pool.query(query, [req.body]);
+        const resp = await pool.query(query, [nuevoUsuario]);
 
         return res.status(200).json({
             success: true,
@@ -118,10 +132,50 @@ const deleteUser = async (req, res) => {
     }
 }
 
+// Params: None
+// Body: Email, Password
+const logIn = async (req, res) => {
+    try {
+        const datos = req.body;
+        const query = `SELECT * FROM user WHERE Email = ?`;
+
+        const [resp] = await pool.query(query, [datos.Email]);
+        const user = resp[0] || null;
+        if (resp) {
+            console.log(resp);
+            bcrypt.compare(datos.Password, user.PasswordHash, (err, isValid) => {
+                if (isValid) {
+                    return res.status(200).json({
+                        success: true,
+                        data: user
+                    });
+                } else {
+                    return res.status(401).json({
+                        success: false,
+                        message: 'Credenciales incorrectas'
+                    });
+                }
+            });
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: 'Usuario no encontrado'
+            });
+        }
+    } catch (error) {
+        console.error('Error logging in:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Error al iniciar sesión'
+        });
+    }
+}
+
 module.exports = {
     getAllUsers,
     getUserById,
     createUser,
     updateUser,
     deleteUser,
+    logIn,
 };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -8,5 +8,6 @@ router.get('/:userId', userController.getUserById);
 router.post('/createUser/', userController.createUser);
 router.put('/updateUser/:userId', userController.updateUser);
 router.delete('/deleteUser/:userId', userController.deleteUser);
+router.post('/logIn/', userController.logIn);
 
 module.exports = router;
